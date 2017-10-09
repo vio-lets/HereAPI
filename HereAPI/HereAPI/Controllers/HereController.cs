e17c6f2dd4766dd16cc8998810dee2244cd2c9ae
clyde Shen <clyde1229@gmail.com>
2017-10-09 22:58:23 +1300

    add Newtonsoft.Json

--- HereAPI/HereAPI/Controllers/HereController.cs
+++ HereAPI/HereAPI/Controllers/HereController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using HereAPI.Classes;
 using HereAPI.Models;
-using Newtonsoft.Json;
-using HereAPI.JSONs.PlacesDiscoverExplore;
 using HereAPI.Services;
 
 namespace HereAPI.Controllers
